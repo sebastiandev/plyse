b534db26637aeb976181e5cfaff4be2e54dcfd82
seb <devsebas@gmail.com>
2016-05-23 15:35:00 -0300

    Update __init__.py
    python 3 import fix

--- plyse/__init__.py
+++ plyse/__init__.py
@@ -1,9 +1,9 @@
-from grammar import GrammarFactory
-from parser import QueryParser
-from query import Query
-from term_parser import *
-from expressions.primitives import *
-from expressions.terms import *
+from .grammar import GrammarFactory
+from .parser import QueryParser
+from .query import Query
+from .term_parser import *
+from .expressions.primitives import *
+from .expressions.terms import *
 
 
 __version__ = '1.0.0'
